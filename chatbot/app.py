72415bed2b818fca8029b8bcfcbe8979b5b2b4fb
Bhavana <63414566+bkanumuri1@users.noreply.github.com>
2025-03-31 13:37:56 -0700

    feat: integrate LangChain memory for contextual generation

--- chatbot/app.py
+++ chatbot/app.py
@@ -2,6 +2,10 @@ from langchain.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.chains import LLMChain
+
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -21,23 +25,28 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
-# PROMPT TEMPLATE
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a renown poet - Sylvia Plath. Please write a 2-line poem about the user's queries"),
-        ("user", "Question:{question}")
-    ]
-)
-
 # streamlit framework
 
 st.title('Let me write a poem for you')
 input_text = st.text_input("What do you want the poem to be about?")
 
+# setup memory for prompts
+messages = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(chat_memory=messages, return_messages=True)
+
+# PROMPT TEMPLATE
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a renowned poet - Sylvia Plath. Based on previous themes, write a 2-line poem about the user's latest query"),
+        ("user", "Question:{question}")
+    ]
+)
+
+
 # openAI LLm
 llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
-output_parser = StrOutputParser()
-chain = prompt | llm | output_parser
+chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
 
 if input_text:
-    st.write(chain.invoke({'question': input_text}))
+    result = chain.run(question=input_text)
+    st.write(result)
